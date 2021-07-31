1e98e518a9248d50fc01e5d89b3656695721e6e3
mohammedmiah67890@gmail.com <mohammedmiah67890@gmail.com>
2021-07-31 22:30:42 +0100

    updated App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,5 @@
-import {BrowserRouter as Router, Route } from "react-router-dom";
+// import logo from './logo.svg';
 import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css";
 import SpacingGrid from './components/SpacingGrid';
 
 function App() {
